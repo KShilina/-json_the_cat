1bff788edfff75d0b9ab07a784fa227852533e61
Ekaterina Shilina <e.shilina@Ekaterinas-MacBook-Pro.local>
2023-02-23 23:48:23 -0500

    Refactor the code by moving the main request logic into a function named fetchBreedDescription.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,18 @@
 const request = require("request");
 
-const argv = process.argv[2];//user input in command line
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${argv}`,
-  function(error, response, body) {
-    if (error) {
-      console.log(error);
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function(error, response, body) {
+      if (error !== null) {
+        return callback(error);
+      }
+      if (JSON.parse(body).length === 0) {
+        return callback(null);
+      }
+      return callback(null, JSON.parse(body)[0].description); //JSON.parse(body)Convert string to object
     }
-    if (JSON.parse(body).length === 0) {
-      console.log("the requested breed is not found.");
-      return;
-    }
+  );
+};
 
-    console.log(JSON.parse(body));
-    console.log("Breed object:", JSON.parse(body)[0].description);//JSON.parse(body)Convert string to object
-  }
-);
+module.exports = { fetchBreedDescription };
