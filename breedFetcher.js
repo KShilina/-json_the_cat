d70b2ae07d3176f01a27937b31191764d6fa8b56
Ekaterina Shilina <e.shilina@Ekaterinas-MacBook-Pro.local>
2023-02-24 14:06:54 -0500

    correct logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,22 @@
 const request = require("request");
 
-const fetchBreedDescription = function(breedName, callback) {
+const fetchBreedDescription = function (breedName, callback) {
   request(
     `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
-    function(error, response, body) {
-      if (error !== null) {
+
+    function (error, response, body) {
+      // console.log("error", error);
+      // console.log('body', body);
+      if (error) {
         return callback(error);
       }
-      if (JSON.parse(body).length === 0) {
-        return callback(null);
+      if (!breedName) {
+        return callback("Please put the name.");
       }
+      if (JSON.parse(body).length === 0) {
+        return callback("Invalid breed name.");
+      }
+
       return callback(null, JSON.parse(body)[0].description); //JSON.parse(body)Convert string to object
     }
   );
